2939ef5b1367f4ed7dae0bcb38f917ade4cf314c
HeinHtetAung <heinhtetaung.itlife@gmail.com>
2018-08-03 10:55:22 +0630

    add aggregate

--- server/controllers/TagController.js
+++ server/controllers/TagController.js
@@ -1,7 +1,26 @@
 const Tag = require('./../models/Tag')
 module.exports = {  
+    // getAllTag: (req, res, next) => {
+	// 	Tag.find({}).then((err, tags)=> {
+    //         if (err)
+    //             res.send(err)
+    //         else if (!tags)
+    //             res.send(404)
+    //         else
+    //             res.send(tags)
+    //         next()            
+    //     })
+
+    // },
+    
     getAllTag: (req, res, next) => {
-		Tag.find({}).then((err, tags)=> {
+        // mongoose aggregate reference from https://gist.github.com/kdelemme/9659364
+        Tag.aggregate([
+            { "$project": {
+                "value": "$_id",
+                "label": "$title",
+            }}
+        ], function (err, tags) {
             if (err)
                 res.send(err)
             else if (!tags)
@@ -9,8 +28,7 @@ module.exports = {
             else
                 res.send(tags)
             next()            
-        })
-
-    },
-    
+        });
+        
+    }
 }
